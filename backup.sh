8154a1854b34e4f4900cbfa4ac4e08e524ffcdd3
Ari Kurniawan <arikurnia2601@gmail.com>
2024-05-07 23:18:52 +0700

    Remove option P while creating tar and capture tar error to log file

--- backup.sh
+++ backup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
-## Usage: backup.sh [options] ARG1
-##
-## Options:
-##   -h, --help    Display this message.
-##   -n            Dry-run; only show what would be done.
-##
+
+###################################################################
+
+#                    CONFIGURATION SECTION                        #
+
+###################################################################
 
 # destination of backup directory
 dest="/root/backup"
@@ -13,12 +13,18 @@ dest="/root/backup"
 source_files="/root/.backup-config.yaml"
 
 # create archive filename.
-curr_date=$(date +'%F')
+curr_date=$(date +'%F_%H-%M-%S')
 hostname=$(hostname -s)
-archive_filename="$hostname-$curr_date.tgz"
+archive_filename="$hostname-$curr_date.tar.gz"
 
 # log files
 log_filename="/root/$hostname-backup.log"
+
+###################################################################
+
+#                          log()	                          #
+
+###################################################################
 
 # send output process to log file
 log() {
@@ -38,13 +44,26 @@ log() {
 	esac
 }
 
+###################################################################
+
+#                          am_i_root()                         	  #
+
+###################################################################
+
 # is script run as root
 am_i_root() {
 	if [[ `id -u` -ne 0 ]]; then
-		echo "Please run this script as root or using sudo!" >&2
+		echo "Please run this script as root!" >&2
+		log "error" "Please run this script as root!"
 		exit 1
 	fi
 }
+
+###################################################################
+
+#                          check_config_file()                    #
+
+###################################################################
 
 # checking config file
 check_config_file() {
@@ -55,15 +74,29 @@ check_config_file() {
 	fi
 }
 
+###################################################################
+
+#                          check_dest_dir()                       #
+
+###################################################################
+
 # checking backup destination dir
 check_dest_dir() {
 	if [[ ! -d $dest ]]; then
 		echo "Directory destination $dest DOES NOT EXIST!!"
 		echo "Creating directory $dest ..."
+		log "info" "Directory destination $dest DOES NOT EXISTS!!"
+		log "info" "Creating directory $dest"
 		sleep 2
 		mkdir $dest
 	fi
 }
+
+###################################################################
+
+#                          checking()                         	  #
+
+###################################################################
 
 # checking before backup process
 checking() {
@@ -72,13 +105,38 @@ checking() {
 	check_dest_dir
 }
 
+###################################################################
+
+#                          databases_dump()                       #
+
+###################################################################
+
+# database dump process
+databases_dump() {
+	local type=$1
+	local username=$2
+	local password=$3
+	local db=$4
+	local dbport=$5
+}
+
+
+###################################################################
+
+#                          backup_process()                       #
+
+###################################################################
+
 # backup process
 backup_process() {
 	backup_files=$(yq .files $source_files)
 	backup_dir=$(yq .directories $source_files)
+	backup_dir=$(yq .databases $source_files)
 	
 	list_files=""
 	list_dirs=""
+	list_db=""
+
 
 	# loop all files then check file is exists
 	for file in $(yq .files[] $source_files); do
@@ -88,7 +146,7 @@ backup_process() {
 		# if file not found send error to log 
 		if [[ ! -f "$file" ]]; then 
 			echo "File Not Found!!"
-			log "error" "File: $file NOT FOUND!!"
+			log "info" "File: $file NOT FOUND!!"
 		else
 			list_files+="$file "
 		fi 
@@ -102,7 +160,7 @@ backup_process() {
 		# if directory not found send error to log
 		if [[ ! -d "$dir" ]]; then 
 			echo "Directory Not Found!!"
-			log "error" "Directory: $dir NOT FOUND!!"
+			log "info" "Directory: $dir NOT FOUND!!"
 		else
 			list_dirs+="$dir/ "
 		fi
@@ -112,18 +170,31 @@ backup_process() {
 	echo -e "\nBacking up ...."
 	date
 	echo
-	    
+
 	# Backup the files using tar.
-	tar -czfP $dest/$archive_filename $list_files $list_dirs 
-	    
+	tar_output=$(tar -czf $dest/$archive_filename $list_files $list_dirs 2>&1)
+
+	# Check the exit status of the tar command
+	if [[ $? -ne 0 ]]; then
+		# Backup failed
+		log "error" "$tar_output"
+
+		echo "tar: Exited with errors!" >&2 
+		echo "Script will exited!" >&2
+		exit 1
+	fi
+
 	# Print end status message.
 	echo
 	if [[ -f $dest/$archive_filename ]]; then
+        	log "success" "Backup completed $dest/$archive_filename"
+
 		echo -e "Backup completed $dest/$archive_filename\n"
 
 		# Long listing of files in $dest to check file sizes.
 		ls -lh $dest/$archive_filename
 	fi
+
 }
 
 checking
